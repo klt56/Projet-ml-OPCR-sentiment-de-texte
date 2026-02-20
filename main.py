6e60e44176282240643d7172d8dc93f2466771d2
klt <letourneurkevin8@gmail.com>
2026-02-20 16:24:00 +0100

    changement main.py

--- main.py
+++ main.py
@@ -252,7 +252,3 @@ def feedback(payload: FeedbackIn):
 
 
 # Lancement local direct : python main.py
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
